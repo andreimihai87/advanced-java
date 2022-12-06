45a2f03f831315311e2419c22621b2df5ec5e1b6
andreimihai87 <andreidocleanu@yahoo.com>
2022-12-07 00:10:53 +0200

    Inserting Data

--- GradleSQLiteProject1/src/main/java/application/App.java
+++ GradleSQLiteProject1/src/main/java/application/App.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,24 +11,42 @@ public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Class.forName("org.sqlite.JDBC");
-        
+
         String dbUrl = "jdbc:sqlite:people.db";
-        
+
         Connection conn = DriverManager.getConnection(dbUrl);
-        
+
         Statement statment = conn.createStatement();
-        
+
         String sql = "create table if not exists user (id integer primary key, name text not null)";
         statment.execute(sql);
+
+        sql = "insert into user (id, name) values (1, 'Andrei')";
+        statment.execute(sql);
+
+        sql = "insert into user (id, name) values (2, 'Mike')";
+        statment.execute(sql);
         
-//        sql = "drop table user";
-//        statment.execute(sql);
-        
-        System.out.println(conn);
-        
+        sql = "insert into user (id, name) values (3, 'Anna')";
+        statment.execute(sql);
+
+        sql = "select * from user";
+        ResultSet resultSet = statment.executeQuery(sql);
+
+        System.out.println("id | name");
+        System.out.println("---------");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            System.out.println(id + "  | " + name);
+        }
+
+        sql = "drop table user";
+        statment.execute(sql);
+
         statment.close();
         conn.close();
-        
+
     }
 
 }
