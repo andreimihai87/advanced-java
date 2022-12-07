5b0e790f052e3a625d7be05d8aeb1ad0646eeefb
andreimihai87 <andreidocleanu@yahoo.com>
2022-12-07 11:06:32 +0200

    Prepared Statments

--- GradleSQLiteProject1/src/main/java/application/App.java
+++ GradleSQLiteProject1/src/main/java/application/App.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,9 @@ import java.sql.Statement;
 public class App {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        int[] ids = { 1, 2, 3 };
+        String[] names = { "John", "Ana", "Joe" };
 
         Class.forName("org.sqlite.JDBC");
 
@@ -21,14 +25,15 @@ public class App {
         String sql = "create table if not exists user (id integer primary key, name text not null)";
         statment.execute(sql);
 
-        sql = "insert into user (id, name) values (1, 'Andrei')";
-        statment.execute(sql);
+        sql = "insert into user (id, name) values (?, ?)";
 
-        sql = "insert into user (id, name) values (2, 'Mike')";
-        statment.execute(sql);
-        
-        sql = "insert into user (id, name) values (3, 'Anna')";
-        statment.execute(sql);
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+        for (int i = 0; i < ids.length; i++) {
+            preparedStatement.setInt(1, ids[i]);
+            preparedStatement.setString(2, names[i]);
+            preparedStatement.execute();
+        }
 
         sql = "select * from user";
         ResultSet resultSet = statment.executeQuery(sql);
